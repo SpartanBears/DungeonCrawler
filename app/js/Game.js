8bd56241b576c541e08a581aa30807e06e4bb997
dvergon <diegoa.vergarag@gmail.com>
2015-11-26 19:49:36 -0300

    No querias algo visual BETO CULIAO!?

--- app/js/Game.js
+++ app/js/Game.js
@@ -19,47 +19,146 @@ function getPrimaryStat(){
 }
 /*TESTING - NOT FINAL CODE*/
 
-function autoPlay(verbose, stepsQ){
+function autoPlayVisual(){
 
-	var character = new Character('warrior', 30);
+	var dungeonContainer = document.createElement('div');
 
-	var steps = [
-		new DungeonStep(0,0,0,"path","entrance",30), new DungeonStep(1,1,0,"path","none",30), new DungeonStep(2,2,0,"door","none",30), 
-		new DungeonStep(3,3,0,"path","none",30), new DungeonStep(4,4,0,"path","none",30), new DungeonStep(5,4,1,"barred_door","none",30), new DungeonStep(5,4,2,"path","none",30),
-		new DungeonStep(5,5,0,"path","exit",30),
-	];
+	document.body.insertBefore(dungeonContainer, document.body.childNodes[0]);
 
-	var d1 = new Dungeon("test", steps);
+	var character = new Character('mage', 50);
+
+	var stepsSize = getStepsXYSize(arraySteps);
+	var stepsX = getStepsXSize(stepsSize);
+	var stepsY = getStepsYSize(stepsSize);
 
 	var auto = new Automaton(character);
-	auto.setEnvironment(d1);
+	auto.setEnvironment(d);
 	auto.initAutomaton();
 
-	document.body.innerHTML = "";
+	dungeonContainer.innerHTML = "";
+	dungeonContainer.appendChild(getVisualXY(stepsX, stepsY, auto.getCurrentStep(), d));
 
-	var log = document.createElement('div');
+	var drawLast = false;
 
-	document.body.insertBefore(log, document.body.childNodes[0]);
+	var run = setInterval(function(){
 
-	if(verbose){
+		auto.walk();
 
-		for(var index = 1; index <= stepsQ; index++){
+		dungeonContainer.innerHTML = "";
+		dungeonContainer.appendChild(getVisualXY(stepsX, stepsY, auto.getCurrentStep(), d));
 
-			var logEntry = document.createElement('span');
-			var content = "["+index+"] " + auto.getCurrentStep().getXYString();
-			
+		if(auto.getCurrentStep().getGateway() == 'exit'){
+
 			auto.walk();
 
-			content += " -> " + auto.getCurrentStep().getXYString() + "<br />";
+			dungeonContainer.innerHTML = "";
+			dungeonContainer.appendChild(getVisualXY(stepsX, stepsY, auto.getCurrentStep(), d));
 
-			logEntry.innerHTML = content;
-
-			log.appendChild(logEntry);
+			clearInterval(run);
 		}
 
-	}else{
+	}, 250);
+	
+}
 
-		//TODO?
+function getVisualXY(sizeX, sizeY, step, dungeon){
+
+	var table = document.createElement('table');
+
+	for(var row = 0; row <= sizeY; row++){
+
+		var tr = document.createElement('tr');
+
+		for(var col = 0; col <= sizeX; col++){
+
+			var td = document.createElement('td');
+			td.id = row+"_"+col;
+
+			td.className = "wall";
+
+			var auxStep = new DungeonStep(0, col, row, "path", "none", 0);
+
+			if(dungeon.isStepExists(auxStep)){
+
+				switch(dungeon.getStepXY(col, row).getType()){
+
+					case 'path':
+
+						td.className = "path";
+
+					break;
+
+					case 'pit':
+
+						td.className = "pit";
+
+					break;
+
+					case 'force_field':
+
+						td.className = "forceField";
+
+					break;
+
+					case 'barred_door':
+
+						td.className = "barredDoor";
+
+					break;
+
+					case 'door':
+
+						td.className = "door";
+
+					break;
+				}
+
+			}
+
+			if(step.getX() == col && step.getY() == row){
+
+				td.className = td.className + " currentPos";
+			}
+
+			tr.appendChild(td);
+		}
+
+		table.appendChild(tr);
 	}
 
+	return table;
 }
+
+//Returns 
+function getStepsXYSize(steps){
+
+	var x = 0;
+	var y = 0;
+
+	for(var index = 0; index < steps.length; index++){
+
+		if(steps[index].getX() > x){
+
+			x = steps[index].getX();
+		}
+
+		if(steps[index].getY() > y){
+
+			y = steps[index].getY();
+		}
+
+	}
+
+	return x + "_" + y;
+}
+
+function getStepsXSize(sizeString){
+
+	return parseInt(sizeString.split("_")[0]);
+}
+
+function getStepsYSize(sizeString){
+
+	return parseInt(sizeString.split("_")[1]);
+}
+
